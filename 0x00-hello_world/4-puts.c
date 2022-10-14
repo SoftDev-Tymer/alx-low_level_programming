910bac93fc1fc5552d0affd75c8bf2121bb0fefb
SoftDev-Tymer <solomonadu4946@gmail.com>
2022-10-14 04:21:46 +0000

    so, this is coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Description: main - Prints string to standard output
+ * Return: Returns 0 if programme runs without errors
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Description: main- Prints string to standard output
+ * Return: Returns 0 upon success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the output is a piece of art,\n");
+	return (0);
+}
